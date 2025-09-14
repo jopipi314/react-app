b7e434b56fb2333a5bdf7ac7d90fe8a06c36af46
joit <jodoit21@stu.jejunu.ac.kr>
2025-09-15 01:40:23 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+// function App() {
+// return <h1>Hello React!</h1>;
+// }
+// export default App;
+
+function App() {
+const name = "React"
+   return (
+  <div>
+  <h1>녕하세요, {name}!</h1>
+  <p>첫 번째 리액트 앱입니다.</p>
+  </div>
+  )
+}
+export default App;
+
